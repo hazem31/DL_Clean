03c58b9478a76e6f5ef1e92a33bae76f0b15f9bb
hazem <hazemnashar31@gmail.com>
2021-01-20 17:05:44 +0200

    Update eval.py

--- dlFramework/eval.py
+++ dlFramework/eval.py
@@ -5,7 +5,7 @@ def accuracy_score(A,Y,thres=0.5):
 
     if Y.shape[0] == 1:
         predictions = (A > thres) * 1
-        acc = float((np.dot(Y,A.T) + np.dot(1-Y,1-predictions.T))/float(Y.size))
+        acc = float((np.dot(Y,predictions.T) + np.dot(1-Y,1-predictions.T))/float(Y.size))
     else:
         index = A.argmax(axis=0)
         yi = Y.argmax(axis=0)
